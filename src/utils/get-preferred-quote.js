198e20d115ec70a5dde188026041ed4944aaa449
Romain <romainc@forestadmin.com>
2022-01-07 12:38:26 +0100

    fix(glimmer-printer): invert quote in mustaches

--- src/utils/get-preferred-quote.js
+++ src/utils/get-preferred-quote.js
@@ -32,4 +32,31 @@ function getPreferredQuote(rawContent, preferredQuoteOrPreferSingleQuote) {
   return preferredQuoteCount > alternateQuoteCount ? alternate : preferred;
 }
 
+// // CP from "../common/util.js"
+// function getPreferredQuote(rawContent, preferredQuote) {
+//   /** @type {{ quote: '"', regex: RegExp, escaped: "&quot;" }} */
+//   const double = { quote: '"', regex: /"/g, escaped: "&quot;" };
+//   /** @type {{ quote: "'", regex: RegExp, escaped: "&apos;" }} */
+//   const single = { quote: "'", regex: /'/g, escaped: "&apos;" };
+
+//   return preferredQuote === "'" ? single : double;
+//   // const alternate = preferred === single ? double : single;
+//   // let result = preferred;
+
+//   // If `rawContent` contains at least one of the quote preferred for enclosing
+//   // the string, we might want to enclose with the alternate quote instead, to
+//   // minimize the number of escaped quotes.
+//   // if (
+//   //   rawContent.includes(preferred.quote) ||
+//   //   rawContent.includes(alternate.quote)
+//   // ) {
+//   //   const numPreferredQuotes = (rawContent.match(preferred.regex) || []).length;
+//   //   const numAlternateQuotes = (rawContent.match(alternate.regex) || []).length;
+
+//   //   result = numPreferredQuotes > numAlternateQuotes ? alternate : preferred;
+//   // }
+
+//   // return result;
+// }
+
 export default getPreferredQuote;
